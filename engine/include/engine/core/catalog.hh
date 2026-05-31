b782266923efceec1650454cd20a58fbca5454d3
Andrew Pogue <atpogue@outlook.com>
2026-05-30 17:15:06 -0700

    feat: catalog data structure

---
+++ engine/include/engine/core/catalog.hh
@@ -0,0 +1,91 @@
+#pragma once
+#include "engine/core/string.hh"
+#include <cassert>
+#include <vector>
+
+template <typename Type>
+concept Named = requires(const Type &type) {
+    { type.name } -> std::convertible_to<std::string_view>;
+};
+
+// An append-only, lookup-by-name set of elements with ID's for quick runtime reference.
+template <Named Type>
+struct Catalog {
+
+    Catalog(u32 capacity)
+        : Catalog()
+    {
+        reserve(capacity);
+    }
+
+    Catalog() = default;
+    Catalog(Catalog &&) noexcept = default;
+    Catalog &operator=(Catalog &&) noexcept = default;
+    Catalog &operator=(const Catalog &) = delete;
+    ~Catalog() noexcept(std::is_nothrow_destructible_v<Type>) = default;
+
+    // Assumptions: name doesn't already exist, maximum size not reached.
+    template <typename... Args>
+    requires std::constructible_from<Type, Args...>
+    u32 emplace(Args &&... args) {
+        assert(data_.size() < UINT32_MAX && "maximum size reached");
+        const u32 id = data_.size();
+        Type &item = data_.emplace_back(std::forward<Args>(args)...);
+        auto [_, added] = lookup_.emplace(item.name, id);
+        assert(added && "emplace on already assigned name");
+        return id;
+    }
+
+    void clear() noexcept(std::is_nothrow_destructible_v<Type>) { data_.clear(); lookup_.clear(); }
+
+    // Assumptions: ID is valid.
+    [[nodiscard]] const Type &operator[](u32 id) const noexcept { assert(has(id)); return data_[id]; }
+    // Assumptions: ID is valid.
+    [[nodiscard]]       Type &operator[](u32 id)       noexcept { assert(has(id)); return data_[id]; }
+
+    // Returns null if the element doesn't exist.
+    [[nodiscard]] const Type *get(std::string_view name) const {
+        auto id = find(name);
+        return id != nil ? &data_[id] : nullptr;
+    }
+
+    // Returns null if the element doesn't exist.
+    [[nodiscard]] Type *get(std::string_view name) {
+        auto id = find(name);
+        return id != nil ? &data_[id] : nullptr;
+    }
+
+    // Returns the ID (index) of the element or nil if not found.
+    [[nodiscard]] u32 find(std::string_view name) const {
+        auto it = lookup_.find(name);
+        return it != lookup_.end() ? it->second : nil;
+    }
+
+    bool has(u32 id) const noexcept { return id < data_.size(); }
+
+    [[nodiscard]] size_t size() const noexcept { return data_.size(); }
+
+    void reserve(u32 n) { data_.reserve(n); lookup_.reserve(n); }
+
+    [[nodiscard]] Catalog copy() const requires std::is_copy_constructible_v<Type> { return *this; }
+
+    using iterator = std::vector<Type>::iterator;
+    using const_iterator = std::vector<Type>::const_iterator;
+
+    [[nodiscard]] const_iterator cbegin() const noexcept { return data_.begin(); }
+    [[nodiscard]] iterator        begin()       noexcept { return data_.begin(); }
+    [[nodiscard]] const_iterator  begin() const noexcept { return cbegin(); }
+
+    [[nodiscard]] const_iterator cend() const noexcept { return data_.end(); }
+    [[nodiscard]] iterator        end()       noexcept { return data_.end(); }
+    [[nodiscard]] const_iterator  end() const noexcept { return cend(); }
+
+private:
+
+    Catalog(const Catalog &) requires std::is_copy_constructible_v<Type> = default;
+
+    std::vector<Type> data_;
+    StringMap<u32> lookup_;
+
+};
+
---
+++ engine/include/engine/core/string.hh
@@ -0,0 +1,9 @@
+#pragma once
+#include "engine/core/hash.hh"
+#include <unordered_map>
+#include <string>
+
+// A transparent hash allows efficient lookup with string-views without constructing a heap-allocated string object during lookup.
+template <typename Type>
+using StringMap = std::unordered_map<std::string, Type, TransparentHash<std::string_view>, std::equal_to<>>;
+
